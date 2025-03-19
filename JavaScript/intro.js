a8a6d63c01f3114fc04d9ba2f9a96327c99bb6fe
Denis Mwanzia <denismwanzia56@gmail.com>
2025-03-19 23:40:28 +0300

    Added content on variable declaration

--- JavaScript/intro.js
+++ JavaScript/intro.js
@@ -1,12 +1,12 @@
 // Variable declaration
-// var keyword
+// var keyword - old way of declaring a variable. Avoid using it
 var name = "Denis";
 console.log(name);
 
-// let keyword
+// let keyword - can be changed
 let age = 27;
 console.log(age);
 
-// const keyword
+// const keyword - cannot be changed
 const tax = 0.345;
 console.log(tax);
